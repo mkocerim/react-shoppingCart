676ccbf4103edfed4571b6ce9f5b245d4e1e054c
Mustafa Koçer <107768698+mkocerim@users.noreply.github.com>
2022-12-08 16:31:24 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./styles.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+const App = (props) => {
+  console.log(">> BOOKLIST", props.bookList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList,
+  };
+};
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/components/Products.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { sepeteEkle } from "../actions/actions";
+
+const Products = (props) => {
+  console.log(">>PRODUCTS BOOKLIST", props.bookList);
+  console.log(">>PRODUCTS CART", props.cart);
+
+  return (
+    <div>
+      <h2>
+        <span>Kitap Listesi</span>
+        <Link to="/cart">Sepetim</Link>
+      </h2>
+      {props.bookList.map((book) => (
+        <div className="book">
+          <img src={book.image} alt={book.name} />
+          <div>
+            <h4>{book.name}</h4>
+            <p>Yazar: {book.author}</p>
+            <p>Fiyat: &#8378; {book.price}</p>
+            <button onClick={() => props.sepeteEkle(book)}>Sepete Ekle</button>
+          </div>
+        </div>
+      ))}
+
+      <div className="book">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
+          alt="Simyaci"
+        />
+        <div>
+          <h4>Simyaci</h4>
+          <p>Yazar: Paulo Coelho</p>
+          <p>Fiyat: &#8378; 19.99</p>
+          <button>Sepete Ekle</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList,
+    cart: state.cart
+  };
+};
+
+export default connect(
+  mapStateToProps, 
+{sepeteEkle}  
+)(Products);
